335b030b426c6153d162c96286bf91443c749fc7
yiyi <3304538614@qq.com>
2023-12-13 10:16:29 +0800

    修改name/侧边栏，  右击暂时不可用

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 function setupContextMenu() {
   chrome.contextMenus.create({
     id: 'define-word',
-    title: 'Define',
+    title: '使用LLM Tagging',
     contexts: ['selection']
   });
 }
@@ -12,11 +12,45 @@ chrome.runtime.onInstalled.addListener(() => {
 });
 
 chrome.contextMenus.onClicked.addListener((data) => {
+  const selectedText = selectText();
+  fetch('http://127.0.0.1:5000/summarize/words/limit_nums', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ "content": selectedText }),
+  })
+    .then(response => {
+      // console.log("response.json() is ", response.json());
+      return response.json();
+    })
+    .then(data => {
+      // Display the returned content in the frontend
+      console.log('Response from the backend:', data);
+      // Handle the data as needed, e.g., update the UI
+      chrome.runtime.sendMessage(
+        {
+          name: "response-summary-to-sidepanel",
+          data: { value: data },
+        },
+        () => {
+          console.log("send response-summary-to-sidepanel");
+        }
+      );
+      // 100ms时间差传递selected-text-to-sidepanel
+      // 因为sidepanel打开之前，sidepanel.js收不到消息/document找不到？
+      chrome.runtime.sendMessage({ type: "open_side_panel" }, () => {
+        //打开侧边栏
+        console.log("send open_side_panel to service-worker.js");
+      });
+    })
+    .catch(error => console.error('Error:', error));
+
   setTimeout(() => {
     chrome.runtime.sendMessage(
       {
         name: "selected-text-to-sidepanel",
-        data: { value: data.selectionText },
+        data: { value: selectText() },
       },
       () => {
         console.log("send selected-text-to-sidepanel");
@@ -29,10 +63,6 @@ chrome.contextMenus.onClicked.addListener((data) => {
     //打开侧边栏
     console.log("send open_side_panel to service-worker.js");
   });
-  // chrome.runtime.sendMessage({
-  //   name: 'define-word',
-  //   data: { value: data.selectionText }
-  // });
 });
 
 chrome.runtime.onMessage.addListener((message, sender) => {
--- sidepanel.js
+++ sidepanel.js
@@ -1,4 +1,3 @@
-//获取selected-text，对应select.js 51行
 chrome.runtime.onMessage.addListener(({ name, data }) => {
   if (name === "selected-text-to-sidepanel") {
     console.log("receive selected-text-to-sidepanel")
@@ -11,9 +10,75 @@ chrome.runtime.onMessage.addListener(({ name, data }) => {
     console.log(document.getElementById("definition-word").innerText);
 
   }
-  else if(name == "response-summary-to-sidepanel"){
+  else if (name == "response-summary-to-sidepanel") {
     console.log("receive response-summary-to-sidepanel")
     document.body.querySelector("#summary-text").innerText = data.value;
 
   }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const copy_button = document.getElementById("sidepanel_copy_button");
+  const display_button = document.getElementById("sidepanel_nodisplay_button");
+  const edit_button = document.getElementById("sidepanel_edit_button");
+  const edit_button2 = document.getElementById("sidepanel_edit_button2");
+  const copy_button2 = document.getElementById("sidepanel_copy_button2");
+
+  // //复制按钮事件绑定
+  // $("#sidepanel_copy_button").on("tap", function () {
+  //   //获取input对象
+  //   var obj = document.getElementById("definition-word").innerText;
+  //   //选择当前对象
+  //   obj.select();
+  //   try {
+  //     //进行复制到剪切板
+  //     if (document.execCommand("Copy", "false", null)) {
+  //       //如果复制成功
+  //       alert("复制成功！");
+  //     } else {
+  //       //如果复制失败
+  //       alert("复制失败！");
+  //     }
+  //   } catch (err) {
+  //     //如果报错
+  //     alert("复制错误！")
+  //   }
+  // })
+
+  display_button.addEventListener("click", function () {
+    console.log("display_button click");
+    if(document.body.querySelector("#text_container").style.display !== "none"){
+      document.body.querySelector("#text_container").style.display = "none";
+    }else{
+      document.body.querySelector("#text_container").style.display = "inline";
+    }
+  });
+  edit_button.addEventListener("click", function () {
+    console.log("edit_button click");
+    console.log(document.body.querySelector("#definition-word").contentEditable)
+    if(document.body.querySelector("#definition-word").contentEditable !== true){
+      document.body.querySelector("#definition-word").contentEditable = true;
+    }else{
+      document.body.querySelector("#definition-word").contentEditable = false;
+    }
+  });
+
+  display_button.addEventListener("click", function () {
+    var e = document.getElementById("definition-word");
+    e.select(); // 选择对象
+    document.execCommand("Copy"); // 执行浏览器复制命令
+  });
+
+  edit_button2.addEventListener("click", function () {
+    console.log("edit_button2 click");
+    console.log(document.body.querySelector("#summary-text").contentEditable)
+    if(document.body.querySelector("#summary-text").contentEditable !== true){
+      document.body.querySelector("#summary-text").contentEditable = true;
+    }else{
+      document.body.querySelector("#summary-text").contentEditable = false;
+    }
+  });
+
+
+
 });
